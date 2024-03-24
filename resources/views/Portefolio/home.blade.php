19c369d1fee6b6bab34909c12910f5cfe29d421f
Max-Scholten <PS229429@summacollege.nl>
2024-03-24 19:22:40 +0100

    first commit

--- resources/views/Portefolio/home.blade.php
+++ resources/views/Portefolio/home.blade.php
@@ -1,6 +1,6 @@
 <x-nav-layout xmlns:X-slot="http://www.w3.org/1999/xlink">
     <x-slot:title>
-        Home
+        Home1
     </x-slot:title>
     <div>
         <div class="text-3xl object-center">
